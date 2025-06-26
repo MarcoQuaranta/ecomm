644c9e57e28718ed12454226019a24db0459e987
marcobiz <marcobiz@hotmail.it>
2025-06-26 10:16:31 +0200

    fix titolo sito

--- src/constant/config.ts
+++ src/constant/config.ts
@@ -1,7 +1,7 @@
 export const siteConfig = {
-  title: 'Next.js + Tailwind CSS + TypeScript Starter',
+  title: 'Erboristeria Urbino',
   description:
-    'A starter for Next.js, Tailwind CSS, and TypeScript with Absolute Import, Seo, Link component, pre-configured with Husky',
+    'Solo prodotti di alta qualità e Made in Italy',
   /** Without additional '/' on the end, e.g. https://theodorusclarence.com */
-  url: 'https://tsnext-tw.thcl.dev',
+  url: 'https://erboristeriaurbino.com',
 };
